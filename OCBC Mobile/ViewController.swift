ac5ab27e4bd7dcd8be391e0c0f2d4996239ff9be
rani <rani@codelabs.co.id>
2021-12-31 11:30:56 +0700

    Initial Commit

---
+++ OCBC Mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCBC Mobile
+//
+//  Created by Paninti Duta Internusa on 31/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
